511270d7b66ae016b2d00dbc85f7eb96d303a942
Kenny <groomkl@gmail.com>
2023-10-16 03:01:37 -0400

    restart code
    Initial Data Generation Script
    This commit includes the initial script for generating synthetic customer data, including customer details, purchase dates, snapshot dates, contract statuses, and financial metrics.
    
    Added Function for Snapshot Date Generation
    This commit adds a function to generate snapshot dates based on purchase dates, ensuring a consistent time period for each customer.
    
    Updated CSV Export of Snapshot Dates
    In this commit, we export the generated snapshot dates to a CSV file, making it easier to reference and reuse this data.
    
    Improved Customer Data Generation
    This commit enhances the customer data generation process by incorporating functions for name, state, vertical, and financial metric generation.
    
    Refactored Contract Status Calculation
    This commit separates the contract status calculation into a distinct function, allowing it to be computed accurately after SnapshotDate and FirstPurchaseDate are populated.
    
    Fixed Contract Status Calculation
    This commit addresses an issue where contract status was consistently 'Active' and updates it correctly based on contract months.
    
    Replaced Snapshot Dates with Pre-generated Dates
    In this commit, we replace the generated snapshot dates with predefined dates to ensure consistency across all customer IDs.
    
    Exported Pre-generated Snapshot Dates to CSV
    Snapshot dates generated based on a fixed time period are now exported to a CSV file for reference and reusability.
    
    Integrated Contract Status Calculation with Data Generation
    Contract status calculation is now integrated into the customer data generation process, ensuring accurate contract statuses for each customer.
    
    Removed Records with Negative Contract Months
    This commit removes records with negative contract months to maintain data accuracy.
    
    Removed Snapshots After Churn Date
    Snapshots occurring after the first churn date are deleted, and MRR is set to negative values for those records.
    
    Added Function for Updating Contract Status and MRR
    A function for updating contract status and MRR is added to better simulate changes in customer accounts.
    
    Fixed Issue with Missing Columns
    This commit addresses an issue where the 'ContractStatus' and 'MRR' columns were missing in the DataFrame.
    
    Revised Data Generation with Added Columns
    The code now ensures that 'ContractStatus' and 'MRR' columns are present in the DataFrame and updates them correctly based on account status.
    
    Finalized Data Generation and Export
    This commit finalizes the data generation process, including customer data, MRR, support tickets, and customer base summaries, and exports the data to CSV files for analysis and reporting.

--- scripts/dataset-gen.py
+++ scripts/dataset-gen.py
@@ -1,26 +1,28 @@
+import os
 import pandas as pd
 import random
 import string
 import numpy as np
-import os
 
-random.seed(a=12345, version=2)
+# Set the data directory path
+data_dir = '/Users/stak/dev/Customer_Success_Dashboard/data'
 
-# Create a function to generate random customer IDs
-def generate_customer_id():
-    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+# Create a function to generate random purchase dates
+def generate_purchase_date():
+    return pd.to_datetime('2020-01-01') + pd.DateOffset(days=random.randint(0, 1200))
 
-# Create a function to generate random first and last names
+# Create a function to generate snapshot dates based on the purchase date
+def generate_snapshot_dates(purchase_date, n_months):
+    snapshot_dates = pd.date_range(start=purchase_date, periods=n_months, freq='M')
+    return snapshot_dates
+
+# Create a function to generate random customer names
 def generate_name():
     first_names = ['Raphael', 'Richard', 'Thomas', 'Ahmed', 'Andre', 'Caden', 'Dallas', 'Eric', 'Ermias', 'Evan', 'Hayden', 'Ismael', 'Jamir', 'Jaylen', 'Kaden', 'Leon', 'Ava', 'Madison', 'Skylar', 'Riley', 'Amelia', 'Olivia', 'Zuri', 'Isabella', 'Fatoumata', 'Serenity', 'Zoey', 'Chloe', 'Aria', 'Fatima', 'Leah', 'Melody', 'Amina', 'Mariam', 'Nova', 'Aaliyah', 'Brielle', 'Layla', 'Mia', 'Grace', 'Nyla', 'Sophia', 'Aisha', 'Kylie', 'Miracle', 'Wynter', 'Autumn', 'Kayla', 'Kehlani', 'Reign', 'Savannah']
     last_names = ['SMITH', 'JOHNSON', 'WILLIAMS', 'BROWN', 'JONES', 'MILLER', 'DAVIS', 'GARCIA', 'RODRIGUEZ', 'WILSON', 'MARTINEZ', 'ANDERSON', 'TAYLOR', 'THOMAS', 'HERNANDEZ', 'MOORE', 'MARTIN', 'JACKSON', 'THOMPSON', 'WHITE', 'LOPEZ', 'LEE']
     first_name = random.choice(first_names)
     last_name = random.choice(last_names)
     return first_name, last_name
-
-# Create a function to generate random purchase dates
-def generate_purchase_date():
-    return pd.to_datetime('2020-01-01') + pd.DateOffset(days=random.randint(0, 1200))
 
 # Function to simulate customer churn or upsell
 def simulate_churn_upsell(row):
@@ -41,11 +43,33 @@ def simulate_churn_upsell(row):
     
     return 'Active'
 
+# Function to generate contract status
+def calculate_contract_status(snapshot_date, purchase_date):
+    contract_months = ((snapshot_date - purchase_date).days // 30) + 1
+    if contract_months < 0:
+        return 'Active'
+    elif contract_months < 12:
+        return 'Active'
+    elif contract_months == 12:
+        return 'Active'
+    else:
+        termination_prob = random.uniform(0.01, 0.2)
+        upsell_prob = random.uniform(0.10, 0.40)
+        downsell_prob = random.uniform(0.10, 0.30)
+        if random.random() < termination_prob:
+            return 'Churned'
+        elif random.random() < upsell_prob:
+            return 'Upsell'
+        elif random.random() < downsell_prob:
+            return 'Downsell'
+        else:
+            return 'Active'
+
 # Function to update contract status and MRR
 def update_contract_status_and_mrr(row):
     # Extract the current contract status and MRR
-    contract_status = row['ContractStatus']
-    current_mrr = row['MRR']
+    contract_status = row.get('ContractStatus', 'Active')
+    current_mrr = row.get('MRR', 0)
 
     # Update contract status based on the existing status
     if contract_status == 'Churned':
@@ -69,180 +93,98 @@ def update_contract_status_and_mrr(row):
 
     return pd.Series({'ContractStatus': new_contract_status, 'MRR': new_mrr})
 
-# Define the project directory
-project_dir = "/Users/stak/dev/Customer_Success_Dashboard"
+# Create a function to generate random verticals
+def generate_vertical():
+    verticals = ['Consultants', 'Non-Profit', 'Entertainment', 'Restaurants', 'Travel']
+    return random.choice(verticals)
 
-# Define the data directory
-data_dir = os.path.join(project_dir, "data")
+# Create a function to generate random MRR
+def generate_mrr():
+    return max(round(np.random.normal(1500, 550), -1), 100)
 
-# Create the project directory if it doesn't exist
-if not os.path.exists(project_dir):
-    os.mkdir(project_dir)
+# Create a function to generate random support tickets
+def generate_support_tickets():
+    return random.randint(0, 10)
 
-# Create the data directory if it doesn't exist
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
+# Create a function to generate random CSAT scores
+def generate_csat():
+    return round(random.uniform(0, 5), 2)
 
-# Define the verticals and their corresponding percentages
-verticals = ['Consultants', 'Non-Profit', 'Entertainment', 'Restaurants', 'Travel']
-vertical_percentages = [0.30, 0.20, 0.10, 0.25, 0.15]
+# Set the number of customers and months
+n_customers = 1000
+n_months = 24
+snp_dte = '2020-01-01'
 
-# Generate customer demographics data
-n_customers = 10000
+snapshot_dates = generate_snapshot_dates(snp_dte,n_months)
 
-# Initialize lists to store customer demographic data
-customers = []
+# Export snapshot dates to a CSV file
+snapshot_dates.to_frame(name='SnapshotDate').to_csv('snapshot_dates.csv', index=False)
 
-# Generate customer detail data for each customer
-for _ in range(n_customers):
-    first_name, last_name = generate_name()
-    purchase_date = generate_purchase_date()
+# Create a data directory if it doesn't exist
+os.makedirs(data_dir, exist_ok=True)
 
-    customer_data = {
-        'CustomerID': generate_customer_id(),
-        'Country': random.choice(['USA', 'Canada', 'UK', 'Australia']),
-        'State': random.choice(['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL',
-            'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MT', 'NE', 'NV', 'NH',
-            'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO',
-            'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', ]),
-        'Vertical': np.random.choice(verticals, p=vertical_percentages),
-        'FirstName': first_name,
-        'LastName': last_name,
-        'FirstPurchaseDate': generate_purchase_date()
-    }
+# Create customer data
+def generate_customer_data(n_customers, snapshot_dates):
+    data = []
 
-    customers.append(customer_data)
+    for _ in range(n_customers):
+        customer_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+        first_name, last_name = generate_name()
+        state = random.choice(['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL',
+                              'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MT', 'NE', 'NV', 'NH',
+                              'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO',
+                              'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'])
+        purchase_date = generate_purchase_date()
+        vertical = generate_vertical()
+        
+        for snapshot_date in snapshot_dates:
+            mrr = generate_mrr()
+            support_tickets = generate_support_tickets()
+            csat = generate_csat()
+            contract_months = ((snapshot_date - purchase_date).days // 30) + 1
+            data.append([customer_id, first_name, last_name, state, purchase_date,snapshot_date,contract_months, vertical, mrr, support_tickets, csat])
 
-# Create a DataFrame for customer demographics
-customers = pd.DataFrame(customers)
-customer_file = os.path.join(data_dir, 'customer_file.csv')
-customers.to_csv(customer_file, index=False)
+    df = pd.DataFrame(data, columns=['CustomerID', 'FirstName', 'LastName', 'State', 'FirstPurchaseDate','SnapshotDate', 'ContractMonths','Vertical',  'MRR', 'SupportTickets', 'AverageCSAT'])
+    return df
 
-# print("Mock customer data demo generated and saved as CSV in the 'data' folder.")
+# Generate customer data
+customer_df = generate_customer_data(n_customers, snapshot_dates)
+customer_df = customer_df[customer_df['ContractMonths'] >= 1]
 
-# Generate monthly snapshots for a year
-n_months = 36
-snapshot_dates = pd.date_range(start='2020-01-01', periods=n_months, freq='M')
+# Check if 'ContractStatus' and 'MRR' columns exist in the DataFrame
+if 'ContractStatus' not in customer_df.columns:
+    customer_df['ContractStatus'] = 'Active'
 
-# Initialize lists to store data
-customer_detail_data = []
-mrr_data = []
-support_tickets_data = []
+if 'MRR' not in customer_df.columns:
+    customer_df['MRR'] = 0
 
-# Generate customer detail data for each customer
-for snapshot_date in snapshot_dates:
-    for _, customer in customers.iterrows():
-        customer_id = customer['CustomerID']
-        customer_state = customer['State']
-        assigned_vertical = customer['Vertical']
+# Update contract status and MRR
+customer_df[['ContractStatus', 'MRR']] = customer_df.apply(update_contract_status_and_mrr, axis=1)
 
-        # Filter out snapshot dates before the customer's FirstPurchaseDate
-        if snapshot_date >= customer['FirstPurchaseDate']:
-            contract_months = ((snapshot_date - customer['FirstPurchaseDate']).days // 30 % 12) + 1
-            contract_status = simulate_churn_upsell({
-                'SnapshotDate': snapshot_date,
-                'FirstPurchaseDate': customer['FirstPurchaseDate']
-            })
+# Calculate contract status
+customer_df['ContractStatus'] = customer_df.apply(lambda row: calculate_contract_status(row['SnapshotDate'], row['FirstPurchaseDate']), axis=1)
 
-            # Create a dictionary with the relevant customer data
-            customer_data = {
-                'SnapshotDate': snapshot_date,
-                'CustomerID': customer_id,
-                'FirstName': customer['FirstName'],
-                'LastName': customer['LastName'],
-                'State': customer_state,
-                'FirstPurchaseDate': customer['FirstPurchaseDate'],
-                'Vertical': assigned_vertical,
-                'ContractMonth': contract_months,
-                'ContractStatus': contract_status
-            }
+# Save customer data to CSV files
+customer_df.to_csv(os.path.join(data_dir, 'customer_detail_data.csv'), index=False)
 
-            # Apply the 'update_contract_status_and_mrr' function to update ContractStatus and MRR
-            updated_data = update_contract_status_and_mrr(customer_data)
+# Create MRR data
+mrr_data = customer_df[['SnapshotDate', 'MRR']].groupby('SnapshotDate').sum().reset_index()
+mrr_data.to_csv(os.path.join(data_dir, 'mrr_data.csv'), index=False)
 
-            # Append the updated data to the customer_detail_data list
-            customer_detail_data.append(updated_data)
+# Create support tickets data
+support_tickets_data = customer_df[['SnapshotDate', 'SupportTickets']].groupby('SnapshotDate').sum().reset_index()
+support_tickets_data.to_csv(os.path.join(data_dir, 'support_tickets_data.csv'), index=False)
 
-            # Generate random MRR and support ticket data with 2 decimal places
-            mrr = max(round(np.random.normal(1000, 550), -1), 100)
-            support_tickets = random.randint(0, 10)
-            average_ticket_score = round(random.uniform(0, 5), 2)
+# Create total customer base summary
+total_customer_base_summary = customer_df.groupby('SnapshotDate')['CustomerID'].count().reset_index()
+total_customer_base_summary.to_csv(os.path.join(data_dir, 'total_customer_base_summary.csv'), index=False)
 
-            mrr_data.append({
-                'SnapshotDate': snapshot_date,
-                'CustomerID': customer_id,
-                'MRR': mrr
-            })
-
-            support_tickets_data.append({
-                'SnapshotDate': snapshot_date,
-                'CustomerID': customer_id,
-                'SupportTickets': support_tickets,
-                'AverageCSAT': average_ticket_score
-            })
-
-# # Create dataframes for customer detail, MRR, and support ticket data
-# customer_detail_data = pd.DataFrame(customer_detail_data)
-# mrr_data = pd.DataFrame(mrr_data)
-# support_tickets_data = pd.DataFrame(support_tickets_data)
-
-# support_tickets_data['TotalCSAT'] = round(support_tickets_data['SupportTickets'] * support_tickets_data['AverageCSAT'],2)
-
-# # Merge 'mrr_data' into 'customer_detail_data'
-# combined_data = customer_detail_data.merge(mrr_data, on=['CustomerID', 'SnapshotDate'], how='left')
-# combined_data = combined_data.merge(support_tickets_data, on=['CustomerID', 'SnapshotDate'], how='left')
-
-# # Apply the update_contract_status_and_mrr function to update the data
-# combined_data[['ContractStatus', 'MRR']] = combined_data.apply(update_contract_status_and_mrr, axis=1)
-
-# # Identify the first churn date for each customer
-# first_churn_dates = combined_data[combined_data['ContractStatus'] == 'Churned'].groupby('CustomerID')['SnapshotDate'].min().reset_index()
-
-# # Merge this information back into the customer_detail_data
-# combined_data = combined_data.merge(first_churn_dates, on='CustomerID', suffixes=('', '_FirstChurn'))
-
-# # Filter out records with a SnapshotDate later than the first churn date
-# combined_data = combined_data[combined_data['SnapshotDate'] <= combined_data['SnapshotDate_FirstChurn']]
-
-# # Clean up by removing unnecessary columns
-# combined_data.drop(columns=['SnapshotDate_FirstChurn'], inplace=True)
-
-# # Save data to CSV in the data directory
-# combined_customer_file = os.path.join(data_dir, 'combined_customer_data.csv')
-# mrr_data.to_csv(os.path.join(data_dir, 'mrr_data.csv'), index=False)
-# support_tickets_data.to_csv(os.path.join(data_dir, 'support_tickets_data.csv'), index=False)
-
-# print("Mock customer success detail for MRR, Support Tickets, and Customer Demo is generated and saved as CSV in the 'data' folder.")
-
-
-# combined_data.to_csv(combined_customer_file, index=False)
-
-# print("Mock combined data generated and saved as CSV in the 'data' folder.")
-
-# # Calculate MRR and Support Tickets summary for the total customer base
-# total_customer_base_summary = combined_data.groupby('SnapshotDate').agg({
-#     'MRR': 'sum',
-#     'SupportTickets': 'sum',
-#     'TotalCSAT': 'sum'
-# }).rename(columns={'MRR': 'Total_MRR', 'SupportTickets': 'Total_SupportTickets','TotalCSAT': 'TotalCSAT'}).reset_index()
-
-# # Calculate MRR and Support Tickets summary for new customers matching the purchase date and snapshot date
-# new_customers_summary = combined_data[combined_data['FirstPurchaseDate'] == combined_data['SnapshotDate']].groupby('SnapshotDate').agg({
-#     'MRR': 'sum',
-#     'SupportTickets': 'sum',
-#     'TotalCSAT': 'sum'
-# }).rename(columns={'MRR': 'New_MRR', 'SupportTickets': 'New_SupportTickets','TotalCSAT': 'New_TotalCSAT'}).reset_index()
-
-# # Save the summary data to CSV
-# total_customer_base_summary.to_csv(os.path.join(data_dir, 'total_customer_base_summary.csv'), index=False)
+# # Create new customers summary
+# new_customers_summary = customer_df[customer_df['ContractStatus'] == 'Active'].groupby('SnapshotDate')['CustomerID'].count().reset_index()
 # new_customers_summary.to_csv(os.path.join(data_dir, 'new_customers_summary.csv'), index=False)
 
-# print("SnapshotDate summary parts saved as CSV in the 'data' folder.")
-
-# # Combine total_customer_base_summary and new_customers_summary
-# combined_summary = total_customer_base_summary.merge(new_customers_summary, on='SnapshotDate', how='left')
-
-# # Save the combined summary data to CSV
+# # Create combined summary
+# combined_summary = pd.merge(total_customer_base_summary, mrr_data, on='SnapshotDate')
+# combined_summary = pd.merge(combined_summary, support_tickets_data, on='SnapshotDate')
+# combined_summary = pd.merge(combined_summary, new_customers_summary, on='SnapshotDate')
 # combined_summary.to_csv(os.path.join(data_dir, 'combined_summary.csv'), index=False)
-
-# print("New customer summary parts saved as CSV in the 'data' folder.")
